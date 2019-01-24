b90c7ffb1557381d3830fd4c90c72112580631d0
David Carlin <david.carlin@cultureamp.com>
2019-01-24 15:20:42 +1100

    Refactor

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,11 +1,16 @@
 class Roman
   def self.to_roman(i)
-    return 'L' + to_roman(i-50) if i >= 50
-    return 'XL' + to_roman(i-40) if i >= 40
-    return 'X' + to_roman(i-10) if i >= 10
-    return 'IX' if i == 9
-    return 'V' + to_roman(i-5) if i >= 5
-    return 'IV' if i == 4
-    'I' * i
+    [
+      ['L', 50],
+      ['XL', 40],
+      ['X', 10],
+      ['IX', 9],
+      ['V', 5],
+      ['IV', 4],
+      ['I', 1],
+    ].each do |roman, arabic|
+      return roman + to_roman(i-arabic) if i >= arabic
+    end
+    ''
   end
 end
